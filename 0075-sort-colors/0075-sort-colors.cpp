9682948cd8521f1cc99fdaf78f49a72694997976
Amrita Maddeshiya <maddeshiyaamrita77@gmail.com>
2026-07-07 11:14:03 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.92%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        // int n = nums.size();
+        // for(int i=0 ; i<n; i++){
+        //     int min_index = i;
+
+        //     for(int j=i+1; j<n; j++){
+        //         if(nums[j] < nums[min_index])
+        //         min_index = j;
+        //     }
+
+        //     swap(nums[i], nums[min_index]);
+        // }
+
+
         int n = nums.size();
-        for(int i=0 ; i<n; i++){
-            int min_index = i;
-
-            for(int j=i+1; j<n; j++){
-                if(nums[j] < nums[min_index])
-                min_index = j;
+        int low=0 , high=n-1 , mid=0;
+        while(mid <= high){
+            if(nums[mid]== 0){
+                swap(nums[mid], nums[low]);
+                mid++, low++;
             }
-
-            swap(nums[i], nums[min_index]);
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
         }
     }
 };
