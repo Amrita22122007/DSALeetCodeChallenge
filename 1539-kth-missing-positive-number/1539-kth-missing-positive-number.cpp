2aaad1f938b4739c1c1d0022f2a0f433345f72fe
Amrita Maddeshiya <maddeshiyaamrita77@gmail.com>
2026-07-17 09:31:54 +0530

    Time: 0 ms (100%), Space: 13.4 MB (29.52%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
+        // int n = arr.size();
+        // int current=1;
+        // int pointer=0;
+        // int missingCount=0;
+        // while(true){
+        // if(pointer<n && arr[pointer]==current){
+        //     pointer++;
+        // }
+        // else{
+        //     missingCount++;
+        //     if(missingCount == k){
+        //         return current;
+        //     }
+        // }
+        // current++;
+
+        // }
+
         int n = arr.size();
+        int missingCount = 0;
+        int pointer =0;
         int current=1;
-        int pointer=0;
-        int missingCount=0;
-        while(true){
-        if(pointer<n && arr[pointer]==current){
-            pointer++;
-        }
-        else{
-            missingCount++;
-            if(missingCount == k){
-                return current;
+        while(missingCount < k){
+            if( pointer<n && arr[pointer]== current){
+                pointer++;
+                current++;
+            }
+            else{
+                missingCount++;
+                current++;
             }
         }
-        current++;
-
-        }
+        return current -1;
     }
 };
