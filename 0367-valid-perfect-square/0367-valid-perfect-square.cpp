fcea29386f7f623caea51fa9ac47d8eae5137086
Amrita Maddeshiya <maddeshiyaamrita77@gmail.com>
2026-07-19 12:25:24 +0530

    Time: 0 ms (100%), Space: 7.6 MB (89.3%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        long long i = 1;
-        while(i*i<= num){
-            if(i*i==num){
-                return true;
-            }
-            i++;
+      //  long long i = 1;
+        // while(i*i<= num){
+        //     if(i*i==num){
+        //         return true;
+        //     }
+        //     i++;
          
-        }
-        return false;
+        // }
+        // return false;
         
+    
+
+    int start = 1 , end = num ;
+
+    while(start<=end){
+        long long mid = start+(end-start)/2;
+        if(mid*mid==num){
+            return true;
+        }
+        else if(mid<num/mid){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+
+       
+       
+    }
+     return false;
     }
     
 };
