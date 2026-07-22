1f322693a61e65c25296418b88a542c2d1cc60b1
Amrita Maddeshiya <maddeshiyaamrita77@gmail.com>
2026-07-22 13:48:25 +0530

    Time: 2 ms (57.75%), Space: 7.7 MB (95.7%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -7,10 +7,10 @@ public:
         
         int start =1 , end=n;
         
-        while(start<end){
+        while(start<=end){
             int mid = start+(end-start)/2;
             if(isBadVersion(mid)){
-                end = mid;
+                end = mid-1;
             }
             else{
                 start= mid+1;
