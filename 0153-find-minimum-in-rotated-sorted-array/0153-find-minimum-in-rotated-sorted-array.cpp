f217be3dd6495a620c22aebf88339a453804aa2d
Amrita Maddeshiya <maddeshiyaamrita77@gmail.com>
2026-07-31 23:11:52 +0530

    Time: 0 ms (100%), Space: 14.3 MB (12.19%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -19,16 +19,14 @@ public:
         }
         while(start<=end){
             int mid = start +(end-start)/2;
-            // if(nums[mid] < nums[mid-1] && nums[mid] < nums[mid+1] && mid<n && mid>0){
-            //     return nums[mid];
-            // }
-            if(mid >0 && nums[mid] < nums[mid-1]){
-                return nums[mid];
-            }
-            //  if(mid <n-1 && nums[mid]< nums[mid+1]){
-            //     return nums[mid];
-            // }
-            else if(nums[mid] >= nums[0]){
+        if(mid >0 && nums[mid]<nums[mid-1]){
+            return nums[mid];
+        }
+
+        if(mid <n-1 && nums[mid] >nums[mid+1]){
+            return nums[mid+1];
+        }
+            else if(nums[mid] >nums[0]){
                 start = mid+1;
             }
             else{
