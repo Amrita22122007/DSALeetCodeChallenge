630426059ed03b5df1954e09addc32f4291451f4
Amrita Maddeshiya <maddeshiyaamrita77@gmail.com>
2026-07-31 22:46:05 +0530

    Time: 0 ms (100%), Space: 14.1 MB (50.98%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-       int n=nums.size();
-       int start=0 , end=n-1;
-       //base case
-       if(nums[0]<=nums[n-1]){
-        return nums[0];
-       }
-       if(n == 1) return nums[0];
-       if(n == 2) return min(nums[0], nums[1]);
-       while(start<=end){
-          int mid= start+(end-start)/2;
-
-           if (mid > 0 && nums[mid - 1] > nums[mid]){
-                return nums[mid];
-           }
-            if (mid < n - 1 && nums[mid] > nums[mid + 1]){
-                return nums[mid + 1];
+        int n = nums.size();
+        int start=0 , end=n-1;
+        if( nums[0] < nums[n-1]){
+            return nums[0];
+        }
+        if(n==1){
+            return nums[0];
+        }
+        if(n==2){
+            if(nums[0] > nums[1]){
+                return nums[1];
             }
-          else if(nums[mid]>nums[0]){
-            start= mid+1;
-          }
-          else{
-            end= mid-1;
-          }
-       } 
-
-       return -1;
+            else{
+                return nums[0];
+            }
+        }
+        while(start<=end){
+            int mid = start +(end-start)/2;
+            // if(nums[mid] < nums[mid-1] && nums[mid] < nums[mid+1] && mid<n && mid>0){
+            //     return nums[mid];
+            // }
+            if(mid >0 && nums[mid] < nums[mid-1]){
+                return nums[mid];
+            }
+            //  if(mid <n-1 && nums[mid]< nums[mid+1]){
+            //     return nums[mid];
+            // }
+            else if(nums[mid] >= nums[0]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+ return -1;
     }
 };
