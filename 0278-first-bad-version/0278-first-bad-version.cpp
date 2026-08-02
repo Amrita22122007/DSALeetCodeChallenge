12d71c116089f4d98f937bdccfdc413c757b6ddf
Amrita Maddeshiya <maddeshiyaamrita77@gmail.com>
2026-08-02 13:29:29 +0530

    Time: 3 ms (16.55%), Space: 7.9 MB (73.77%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,18 +4,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        
-        int start =1 , end=n;
-        
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            if(isBadVersion(mid)){
-                end = mid-1;
-            }
-            else{
-                start= mid+1;
-            }
+      int start =1 , end =n;
+      while(start<=end){
+        int mid = start +(end-start)/2;
+        if(isBadVersion(mid)){
+            end = mid-1;
         }
-        return start;
+        else{
+            start = mid+1;
+        }
+      }  
+      return start;
     }
 };
