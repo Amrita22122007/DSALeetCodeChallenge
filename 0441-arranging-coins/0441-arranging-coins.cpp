7b8fdbf772a9978a843fe4d7ec6be35a407516f1
Amrita Maddeshiya <maddeshiyaamrita77@gmail.com>
2026-08-05 22:25:18 +0530

    Time: 1 ms (44.51%), Space: 8.9 MB (37.79%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,12 +1,31 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int coins = n;
-        int row =1;
-        while(coins>=row){
-              coins = coins-row;
-              row++;
+        // int coins = n;
+        // int row =1;
+        // while(coins>=row){
+        //       coins = coins-row;
+        //       row++;
+        // }
+        // return row-1;
+
+
+        long long start =1 , end = n;
+        int  ans = 0;
+        while(start<=end){
+            long long mid = start + (end-start)/2;
+            long long coinNeeded = mid*(mid+1)/2;
+
+            if(coinNeeded <= n){
+            ans = mid;
+            start = mid+1;     
+                   }       
+            else{
+                end = mid-1;
+            }
+            
         }
-        return row-1;
+        
+        return ans;
     }
 };
